63d24c5c575af4c1cfd499fb33a73d7feab92dfe
Dmytro <fedongm@gmail.com>
2020-09-28 21:56:55 +0200

    Init

---
+++ src/main/java/org/fedon/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package org.fedon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmytro Fedonin
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+	}
+}
